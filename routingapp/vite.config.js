084f30d1a82c2948a7e814a479f881e269e2038b
heian2704 <heinthant2704@gmail.com>
2024-08-15 20:04:39 +0700

    Added a routing app

---
+++ routingapp/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/routing-app/', // Ensure this matches your deployment setup
+});
